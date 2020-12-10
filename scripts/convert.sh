c982d3731bee291bda21b5396892f6dcccabcbd4
Lois Duplain <lduplain@z3r5p5.42lyon.fr>
2020-12-10 17:09:15 +0100

    Set conversion target variable

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,9 +1,11 @@
-rm -rf libft-converted
-mkdir libft-converted
-find includes -type f -name "*.h" -exec cp -n {} libft-converted/ \;
-find sources -type f -name "*.c" -exec cp -n {} libft-converted/ \;
-tail -n +2 "./Makefile" > "./libft-converted/tailed-makefile"
-find libft-converted -name '*.c' | awk -F'/' '{print $2}' | sed 's/^/SRCS += /' > "./libft-converted/srcs-makefile"
-cat "./libft-converted/srcs-makefile" "./libft-converted/tailed-makefile" > "./libft-converted/Makefile"
-rm "./libft-converted/tailed-makefile"
-rm "./libft-converted/srcs-makefile"
+TARGET="./libft-convertedddddddd"
+
+rm -rf $TARGET
+mkdir $TARGET
+find includes -type f -name "*.h" -exec cp -n {} $TARGET/ \;
+find sources -type f -name "*.c" -exec cp -n {} $TARGET \;
+tail -n +2 "./Makefile" > $TARGET"/tailed-makefile"
+find libft-converted -name '*.c' | awk -F'/' '{print $2}' | sed 's/^/SRCS += /' > $TARGET"/srcs-makefile"
+cat $TARGET"/srcs-makefile" $TARGET"/tailed-makefile" > $TARGET"/Makefile"
+rm $TARGET"/tailed-makefile"
+rm $TARGET"/srcs-makefile"
