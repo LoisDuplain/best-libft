a8e19ca03fc3de8653744625acf0789b0d21154e
Umetsuno <heldenwachs@gmail.com>
2021-02-08 10:49:01 +0100

    update : color directory for normv2

--- sources/color/color_to_int.c
+++ sources/color/color_to_int.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   color_to_int.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lduplain <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: faherrau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 11:28:45 by lduplain          #+#    #+#             */
-/*   Updated: 2021/01/14 12:00:14 by lduplain         ###   ########lyon.fr   */
+/*   Updated: 2021/02/08 10:48:38 by faherrau         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		color_to_int(t_color color)
+int	color_to_int(t_color color)
 {
 	return ((255 - color.a) << 24 | color.r << 16 | color.g << 8 | color.b);
 }
