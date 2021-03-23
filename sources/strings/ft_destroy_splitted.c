e4be3ed736ac227ae853a058b1e163f0ae41f4e2
Lois Duplain <lduplain@z4r3p1.42lyon.fr>
2021-03-23 15:47:39 +0100

    Fix segfault in ft_destroy_splitted if (char **)splitted was NULL

--- sources/strings/ft_destroy_splitted.c
+++ sources/strings/ft_destroy_splitted.c
@@ -6,7 +6,7 @@
 /*   By: lduplain <lduplain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 18:56:49 by lduplain          #+#    #+#             */
-/*   Updated: 2021/03/23 12:22:39 by lduplain         ###   ########lyon.fr   */
+/*   Updated: 2021/03/23 15:46:40 by lduplain         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,17 @@ void	*ft_destroy_splitted(char **splitted)
 {
 	int		word_index;
 
-	word_index = 0;
-	while (splitted[word_index])
+	if (splitted != NULL)
 	{
-		free(splitted[word_index]);
-		splitted[word_index] = NULL;
-		word_index++;
+		word_index = 0;
+		while (splitted[word_index])
+		{
+			free(splitted[word_index]);
+			splitted[word_index] = NULL;
+			word_index++;
+		}
+		free(splitted);
+		splitted = NULL;
 	}
-	free(splitted);
-	splitted = NULL;
 	return (NULL);
 }
