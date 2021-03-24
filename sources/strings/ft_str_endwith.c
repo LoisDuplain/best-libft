96bd0d28ec852c8464203b0d675a573c6ff2b51a
Lois Duplain <lduplain@z4r3p3.42lyon.fr>
2021-03-24 17:34:50 +0100

    debug

--- sources/strings/ft_str_endwith.c
+++ sources/strings/ft_str_endwith.c
@@ -6,7 +6,7 @@
 /*   By: lduplain <lduplain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 16:44:40 by lduplain          #+#    #+#             */
-/*   Updated: 2021/03/24 16:53:34 by lduplain         ###   ########lyon.fr   */
+/*   Updated: 2021/03/24 17:34:03 by lduplain         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ t_bool	ft_str_endwith(char *str, char *end_with)
 	if (end_with_len > str_len)
 		return (FALSE);
 	score = 0;
-	while (score < end_with_len)
+	while (score != end_with_len)
 	{
 		if (end_with[end_with_len - score] != str[str_len - score])
 			return (FALSE);
