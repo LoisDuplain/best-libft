a5dd1bdb0ff63c0602a20167e35862c89031a5a5
Lois Duplain <lduplain@z3r7p3.42lyon.fr>
2021-03-30 09:16:02 +0200

    debug

--- sources/strings/ft_destroy_splitted.c
+++ sources/strings/ft_destroy_splitted.c
@@ -6,7 +6,7 @@
 /*   By: lduplain <lduplain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 18:56:49 by lduplain          #+#    #+#             */
-/*   Updated: 2021/03/30 09:13:38 by lduplain         ###   ########lyon.fr   */
+/*   Updated: 2021/03/30 09:15:42 by lduplain         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,12 @@ void	*ft_destroy_splitted(char ***splitted)
 		puts("ft_destroy_splitted - b");
 		word_index = 0;
 		puts("ft_destroy_splitted - c");
-		while (*splitted[word_index])
+		while ((*splitted)[word_index])
 		{
 			puts("ft_destroy_splitted - d");
-			free(*splitted[word_index]);
+			free((*splitted)[word_index]);
 			puts("ft_destroy_splitted - e");
-			*splitted[word_index] = NULL;
+			(*splitted)[word_index] = NULL;
 			puts("ft_destroy_splitted - f");
 			word_index++;
 			puts("ft_destroy_splitted - g");
