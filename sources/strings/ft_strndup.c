ef7189006ecae65d9408e93b0d6d75c8f4c250b7
Lois Duplain <lduplain@z3r11p3.42lyon.fr>
2021-03-31 19:43:59 +0200

    debug

--- sources/strings/ft_strndup.c
+++ sources/strings/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: lduplain <lduplain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 16:51:08 by lduplain          #+#    #+#             */
-/*   Updated: 2021/03/31 19:40:31 by lduplain         ###   ########lyon.fr   */
+/*   Updated: 2021/03/31 19:43:40 by lduplain         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strndup(const char *str, size_t n, t_bool free_str)
 
 	if (!str)
 		return (NULL);
-	result = ft_calloc(n + 1, sizeof(char));
+	result = ft_calloc(n + 1, sizeof(*str));
 	if (result == NULL)
 		return (NULL);
 	ft_bzero(result, n + 1);
