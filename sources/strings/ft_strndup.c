7792ff6fed160c2395a1db8aa8942cb01152a2f0
Lois Duplain <lduplain@z3r11p3.42lyon.fr>
2021-03-31 19:40:58 +0200

    debug

--- sources/strings/ft_strndup.c
+++ sources/strings/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: lduplain <lduplain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 16:51:08 by lduplain          #+#    #+#             */
-/*   Updated: 2021/03/31 19:28:59 by lduplain         ###   ########lyon.fr   */
+/*   Updated: 2021/03/31 19:40:31 by lduplain         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_strndup(const char *str, size_t n, t_bool free_str)
 		return (NULL);
 	ft_bzero(result, n + 1);
 	index = 0;
-	while (str[index] && index < n + 1)
+	while (str[index] && index < n)
 	{
 		result[index] = str[index];
 		index++;
