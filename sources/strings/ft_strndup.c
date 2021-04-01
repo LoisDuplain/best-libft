1c357d92bed7c9d4f9cf944302ceafd609b75f65
Lois Duplain <lduplain@z3r11p3.42lyon.fr>
2021-04-01 11:46:46 +0200

    debug

--- sources/strings/ft_strndup.c
+++ sources/strings/ft_strndup.c
@@ -6,11 +6,11 @@
 /*   By: lduplain <lduplain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 16:51:08 by lduplain          #+#    #+#             */
-/*   Updated: 2021/04/01 11:34:03 by lduplain         ###   ########lyon.fr   */
+/*   Updated: 2021/04/01 11:46:34 by lduplain         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+/* #include "libft.h"
 
 char	*ft_strndup(const char *str, size_t n, t_bool free_str)
 {
@@ -31,6 +31,35 @@ char	*ft_strndup(const char *str, size_t n, t_bool free_str)
 		index++;
 	}
 	result[index] = 0;
+	printf("Char: %zu | *Str: %zu | Result: {%s} | Size: %zu\n", sizeof(char), sizeof(*str), result, n);
+	if (free_str)
+		free((char *)str);
+	return (result);
+} */
+
+#include "libft.h"
+
+char	*ft_strndup(const char *str, size_t n, t_bool free_str)
+{
+	size_t	index;
+	char	*result;
+
+	if (!str)
+		return (NULL);
+	result = ft_calloc(n + 1, sizeof(*str));
+	if (result == NULL)
+		return (NULL);
+	index = 0;
+	while (str[index] && index < n)
+	{
+		result[index] = str[index];
+		index++;
+	}
+	while (index <= n)
+	{
+		result[index] = 0;
+		index++;
+	}
 	printf("Char: %zu | *Str: %zu | Result: {%s} | Size: %zu\n", sizeof(char), sizeof(*str), result, n);
 	if (free_str)
 		free((char *)str);
