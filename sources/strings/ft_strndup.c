a0a829fd219b142c547e8524626d0276f25c073c
Lois Duplain <lduplain@z3r11p3.42lyon.fr>
2021-04-01 11:12:34 +0200

    debug

--- sources/strings/ft_strndup.c
+++ sources/strings/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: lduplain <lduplain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 16:51:08 by lduplain          #+#    #+#             */
-/*   Updated: 2021/04/01 11:08:57 by lduplain         ###   ########lyon.fr   */
+/*   Updated: 2021/04/01 11:12:09 by lduplain         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ char	*ft_strndup(const char *str, size_t n, t_bool free_str)
 	if (!str)
 		return (NULL);
 	result = ft_calloc(n + 1, sizeof(*str));
-	printf("Char: %zu | *Str: %zu\n", sizeof(char), sizeof(*str));
 	if (result == NULL)
 		return (NULL);
 	ft_bzero(result, n + 1);
@@ -31,6 +30,7 @@ char	*ft_strndup(const char *str, size_t n, t_bool free_str)
 		index++;
 	}
 	result[index] = 0;
+	printf("Char: %zu | *Str: %zu | Result: {%s}\n", sizeof(char), sizeof(*str), result);
 	if (free_str)
 		free((char *)str);
 	return (result);
