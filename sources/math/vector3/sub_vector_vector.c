8b72baefaae20cc25ad09ef9fd8c5ddafa1e2507
Lois Duplain <loisdupl42@gmail.com>
2021-04-21 12:38:55 +0200

    Fix sub_vector_vector function

--- sources/math/vector3/sub_vector_vector.c
+++ sources/math/vector3/sub_vector_vector.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sub_vector_vector.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lduplain <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: lduplain <lduplain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 17:51:16 by lduplain          #+#    #+#             */
-/*   Updated: 2021/01/14 18:26:57 by lduplain         ###   ########lyon.fr   */
+/*   Updated: 2021/04/21 12:38:18 by lduplain         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 t_vector3	sub_vector_vector(t_vector3 vector, t_vector3 to_sub)
 {
-	return (sub_vector_coord(vector, to_sub.vx, to_sub.vx, to_sub.vx));
+	return (sub_vector_coord(vector, to_sub.vx, to_sub.vy, to_sub.vz));
 }
