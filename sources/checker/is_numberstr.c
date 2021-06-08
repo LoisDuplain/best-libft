12272ac08a4856d7fec6dd5bb4f0f2ba7a2cc1c9
Lois Duplain <lduplain@z4r3p5.42lyon.fr>
2021-06-08 16:04:01 +0200

    Fix is_numberstr function

--- sources/checker/is_numberstr.c
+++ sources/checker/is_numberstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   is_numberstr.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lduplain <lduplain@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lduplain <lduplain@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 16:24:52 by lduplain          #+#    #+#             */
-/*   Updated: 2021/05/11 16:32:17 by lduplain         ###   ########lyon.fr   */
+/*   Updated: 2021/06/08 16:02:08 by lduplain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ t_bool	is_numberstr(char *str)
 	i = 0;
 	while (str[i] && (str[i] == '-' || str[i] == '+'))
 		i++;
+	if (i == ft_strlen(str))
+		return (FALSE);
 	while (str[i])
 	{
 		if (!is_numeric(str[i]))
